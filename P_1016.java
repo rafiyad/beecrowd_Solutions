55a4e72d7f7a3c801f84f764f501cfc5a69d9588
Rafiyad Darazat <141349190+rafiyad@users.noreply.github.com>
2023-09-02 17:17:52 +0600

    Add files via upload

---
+++ P_1016.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class P_1016 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n=in.nextInt();
+        System.out.println(n*2+" minutos");
+    }
+}
